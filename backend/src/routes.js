ce7d5ddbc993235998d6f519b420db555b455746
Léu Almeida <leo@webid.net.br>
2019-10-01 19:09:10 -0300

    Create Routes

---
+++ backend/src/routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const SessionController = require('./controllers/SessionController')
+
+const routes = express.Router();
+
+routes.post('/sessions', SessionController.store);
+
+module.exports = routes;
