ae05bebf77d486bf72d1f0d301a1c71ac358d0a9
Léu Almeida <leo@webid.net.br>
2019-10-01 21:26:48 -0300

    Criação do Controller da Session

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -1,5 +1,15 @@
+const User = require('../models/User')
+
 module.exports = {
-    store(req, res){
-        return res.json({ message: 'Hello World'})
+    async store(req, res) {
+        const { email } = req.body;
+
+        let user = await User.findOne({ email });
+
+        if (!user) {
+            user = await User.create({ email });
+        }
+
+        return res.json(user);
     }
 };
