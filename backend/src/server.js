951e7250ad6a055872de4cad30ba3b79afab5d14
Léu Almeida <leo@webid.net.br>
2019-10-02 14:45:19 -0300

    Update server with CORS and thumbnail views in /files

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
@@ -16,7 +18,9 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-bsste.mongodb.net/
 // req.params = Acessar route params (para edição e delete)
 // req.body = Acessar corpo da requisição (para criação e edição)
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
