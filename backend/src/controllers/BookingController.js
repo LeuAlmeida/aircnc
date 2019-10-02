20a792a8e5b137cec3ccb2dbdd94775a87571940
Léu Almeida <leo@webid.net.br>
2019-10-01 21:26:27 -0300

    Criação do Controller de Reservas

---
+++ backend/src/controllers/BookingController.js
@@ -0,0 +1,19 @@
+const Booking = require('../models/Booking');
+
+module.exports = {
+    async store(req, res) {
+        const { user_id } = req.headers;
+        const { spot_id } = req.params;
+        const { date } = req.body;
+
+        const booking = await Booking.create({
+            user: user_id,
+            spot: spot_id,
+            date
+        });
+
+        await booking.populate('spot').populate('user').execPopulate();
+
+        return res.json(booking);
+    }
+};
