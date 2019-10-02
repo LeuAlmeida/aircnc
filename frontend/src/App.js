64c8be213113fb735f2aab2834f295e095e3d784
Léu Almeida <leo@webid.net.br>
2019-10-02 14:49:10 -0300

    App.js cleaner (remove Login page)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,20 @@
 import React from 'react';
+
 import './App.css';
 
-import logo from './assets/logo.svg'
+import logo from './assets/logo.svg';
+
+import Routes from './routes';
 
 function App() {
+  
+
   return (
     <div className="container">
       <img src={logo} alt="AirCnC" />
 
       <div className="content">
-        <p>
-          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
-        </p>
-
-        <form>
-          <label htmlFor="email">E-MAIL *</label>
-          <input
-            type="email"
-            id="email"
-            placeholder="Seu melhor e-mail"
-          />
-
-          <button type="submit" className="btn">Entrar</button>
-        </form>
+        <Routes />
       </div>
     </div>
   );
