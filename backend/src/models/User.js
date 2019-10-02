5d1d333651fa752e389434a98be9f84e7b1e5562
Léu Almeida <leo@webid.net.br>
2019-10-01 21:27:37 -0300

    Correções de digitação

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    email: String
-})
+    email: String,
+});
 
 module.exports = mongoose.model('User', UserSchema);
