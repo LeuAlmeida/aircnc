edfe95896d6667f1f9fcd11ed2ddbdc67e9e4f0b
Léu Almeida <leo@webid.net.br>
2019-10-01 21:27:51 -0300

    Atualização nas Rotas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,22 @@
 const express = require('express');
-const SessionController = require('./controllers/SessionController')
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
+
+const SessionController = require('./controllers/SessionController');
+const SpotController = require('./controllers/SpotController');
+const DashboardController = require('./controllers/DashboardController');
+const BookingController = require('./controllers/BookingController');
 
 const routes = express.Router();
+const upload = multer(uploadConfig);
 
 routes.post('/sessions', SessionController.store);
+
+routes.get('/spots', SpotController.index);
+routes.post('/spots', upload.single('thumbnail'), SpotController.store);
+
+routes.get('/dashboard', DashboardController.show);
+
+routes.post('/spots/:spot_id/bookings', BookingController.store);
 
 module.exports = routes;
