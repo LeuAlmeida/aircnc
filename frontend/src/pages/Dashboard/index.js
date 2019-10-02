e3216e0577d9f25919803b6da1c5377fac9dfcdf
Léu Almeida <leo@webid.net.br>
2019-10-02 15:23:22 -0300

    Update Dashboard with price (if free), button "create new" and import stylesheets

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom'
 import api from '../../services/api';
+
+import './styles.css'
 
 export default function Dashboard() {
     const [spots, setSpots]  = useState([]);
@@ -24,10 +27,16 @@ export default function Dashboard() {
                     <li key={spot._id}>
                         <header style={{ backgroundImage: `url(${spot.thumbnail_url})` }} />
                         <strong>{spot.company}</strong>
-                        <span>{spot.price}</span>
+                        <span>{spot.price ? `R$${spot.price}/dia` : 'GRATUITO'}</span>
                     </li>
                 ))}
             </ul>
+
+            <Link to="/new">
+                <button className="btn">
+                    Cadastrar novo spot
+                </button>
+            </Link>
         </>
     )
 }
