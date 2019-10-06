b01ab8e04ddaf142b0427ffcaa614afdea53aacb
Léu Almeida <leo@webid.net.br>
2019-10-06 20:41:11 -0300

    Update App.js

--- mobile/App.js
+++ mobile/App.js
@@ -1,19 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Routes from './src/routes';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-    </View>
-  );
+  return <Routes />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
