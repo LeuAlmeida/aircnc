9159f66d2084c2beda630c153cb8e3a09e2b5bb8
Léu Almeida <leo@webid.net.br>
2019-10-06 21:40:25 -0300

    Update Spot List view

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,6 +1,39 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, StyleSheet, Image, AsyncStorage } from 'react-native';
+
+import SpotList from '../components/SpotList'
+
+import logo from '../assets/logo.png';
 
 export default function List() {
-    return <View />
+    const [techs, setTechs] = useState([]);
+
+    useEffect(() => {
+        AsyncStorage.getItem('techs').then(storagedTechs => {
+            const techsArray = storagedTechs.split(',').map(tech => tech.trim());
+
+            setTechs(techsArray);
+        })
+    }, []);
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Image style={styles.logo} source={logo} />
+
+            {techs.map(tech => <SpotList key={tech} tech={tech} /> )}
+        </SafeAreaView>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+
+    logo: {
+        height: 32,
+        resizeMode: 'contain',
+        alignSelf: 'center',
+        marginTop: 10
+    }
+})
