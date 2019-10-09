1daf2d113b049efa67427406c5de606e78efc7ab
Léu Almeida <leo@webid.net.br>
2019-10-09 00:28:39 -0300

    Update YellowBox errors

--- mobile/App.js
+++ mobile/App.js
@@ -3,7 +3,9 @@ import { YellowBox } from 'react-native';
 import Routes from './src/routes';
 
 YellowBox.ignoreWarnings([
-  'Unrecognized WebSocket'
+  'Unrecognized WebSocket',
+  'Warning: Failed prop type:',
+  'Warning: State updates from the useState()'
 ]);
 
 export default function App() {
