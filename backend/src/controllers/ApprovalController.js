39ad5b99973e82c2b5aadc9c62e2eb4eac4b701c
Léu Almeida <leo@webid.net.br>
2019-10-08 22:33:45 -0300

    Create Approval Controller on backend

---
+++ backend/src/controllers/ApprovalController.js
@@ -0,0 +1,15 @@
+const Booking = require('../models/Booking');
+
+module.exports = {
+    async StorageEvent(req, res) {
+        const { booking_id } = req.params;
+
+        const booking = await Booking.findById(booking_id).populate('spot');
+        
+        booking.approved = true;
+
+        await booking.save();
+
+        return res.json(booking);
+    }
+};
