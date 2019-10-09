bb6409adc364e918dba5dc8d9f394229aa499c5b
Léu Almeida <leo@webid.net.br>
2019-10-08 23:24:40 -0300

    Update spot list

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, Image, AsyncStorage } from 'react-native';
+import socketio from 'socket.io-client'
+import { Alert, SafeAreaView, ScrollView, StyleSheet, Image, AsyncStorage } from 'react-native';
 
 import SpotList from '../components/SpotList'
 
@@ -7,6 +8,18 @@ import logo from '../assets/logo.png';
 
 export default function List() {
     const [techs, setTechs] = useState([]);
+
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user_id => {
+            const socket = socketio('http://192.168.15.11:3333', {
+                query: { user_id }
+            })
+
+            socket.on('booking_response', booking => {
+                Alert.alert(`Sua reserva em ${booking.spot.company} em ${booking.date} foi ${booking.approver ? 'APROVADA' : 'REJEITADA'}`)
+            })
+        })
+    }, []);
 
     useEffect(() => {
         AsyncStorage.getItem('techs').then(storagedTechs => {
